1556e41427b73db218dac04edb9ba06ba5fce998
Chia-Hsiang Cheng <gary.chiahsiang.cheng@gmail.com>
2024-06-01 12:39:31 +0800

    Document code

--- src/include/allocators.hpp
+++ src/include/allocators.hpp
@@ -5,30 +5,150 @@
 
 namespace ChiaVec {
 namespace Memory {
+/**
+ * @brief Base class for memory allocators.
+ *
+ * This class defines the interface for memory allocators that can be used to
+ * allocate, release, and resize memory, as well as copy data between host and
+ * device memory.
+ *
+ * @tparam OnHost Indicates whether the allocator allocates memory on the host
+ * (CPU) or on the device (GPU).
+ */
 template <bool OnHost>
 struct BaseAllocator {
+  /**
+   * @brief Indicates whether the allocator allocates memory on the host (CPU)
+   * or on the device (GPU).
+   */
   static constexpr bool AllocatesOnHost = OnHost;
+
+  /**
+   * @brief Allocates memory of the specified size.
+   *
+   * @param size The size of the memory block to allocate, in bytes.
+   * @return A pointer to the allocated memory block.
+   */
   virtual void *operator()(std::size_t size) = 0;
+
+  /**
+   * @brief Releases the memory block pointed to by the given pointer.
+   *
+   * @param ptr A pointer to the memory block to be released.
+   */
   virtual void release(void *ptr) = 0;
+
+  /**
+   * @brief Resizes the memory block pointed to by the given pointer.
+   *
+   * @param ptr The pointer to the memory block to be resized.
+   * @param oldSize The current size of the memory block, in bytes.
+   * @param newSize The new size of the memory block, in bytes.
+   * @return A pointer to the resized memory block.
+   */
   virtual void *resize(void *ptr, std::size_t oldSize, std::size_t newSize) = 0;
+
+  /**
+   * @brief Copies data between host and device memory.
+   *
+   * @param dst The destination pointer for the copy operation.
+   * @param src The source pointer for the copy operation.
+   * @param size The size of the data to be copied, in bytes.
+   * @param dstOnHost Indicates whether the destination memory is on the host.
+   * @param srcOnHost Indicates whether the source memory is on the host.
+   */
   virtual void copy(void *dst, const void *src, std::size_t size,
                     bool dstOnHost, bool srcOnHost) = 0;
 };
 
+/**
+ * @brief Default memory allocator that allocates memory on the host (CPU).
+ *
+ * This class implements the BaseAllocator interface to provide a default
+ * memory allocator that allocates memory on the host (CPU).
+ */
 struct DefaultAllocator : BaseAllocator<true> {
+  /**
+   * @brief Allocates a block of memory of the specified size on the host.
+   *
+   * @param size The size of the memory block to allocate, in bytes.
+   * @return A pointer to the allocated memory block.
+   */
   virtual void *operator()(std::size_t size) override;
+
+  /**
+   * @brief Releases the memory block pointed to by the given pointer.
+   *
+   * @param ptr A pointer to the memory block to be released.
+   */
   virtual void release(void *ptr) override;
+
+  /**
+   * @brief Resizes the memory block pointed to by the given pointer.
+   *
+   * @param ptr The pointer to the memory block to be resized.
+   * @param oldSize The current size of the memory block, in bytes.
+   * @param newSize The new size of the memory block, in bytes.
+   * @return A pointer to the resized memory block.
+   */
   virtual void *resize(void *ptr, std::size_t oldSize,
                        std::size_t newSize) override;
+
+  /**
+   * @brief Copies data between host and device memory.
+   *
+   * @param dst The destination pointer for the copy operation.
+   * @param src The source pointer for the copy operation.
+   * @param size The size of the data to be copied, in bytes.
+   * @param dstOnHost Indicates whether the destination memory is on the host.
+   * @param srcOnHost Indicates whether the source memory is on the host.
+   */
   virtual void copy(void *dst, const void *src, std::size_t size,
                     bool dstOnHost, bool srcOnHost) override;
 };
 
+/**
+ * @brief Default memory allocator that allocates memory on the device (GPU).
+ *
+ * This class implements the BaseAllocator interface to provide a default
+ * memory allocator that allocates memory on the device (GPU).
+ */
 struct DefaultCudaAllocator : BaseAllocator<false> {
+  /**
+   * @brief Allocates a block of memory of the specified size on the device.
+   *
+   * @param size The size of the memory block to allocate, in bytes.
+   * @return A pointer to the allocated memory block.
+   */
   virtual void *operator()(std::size_t size) override;
+
+  /**
+   * @brief Releases the memory block pointed to by the given pointer.
+   *
+   * @param ptr A pointer to the memory block to be released.
+   */
   virtual void release(void *ptr) override;
+
+  /**
+   * @brief Resizes the memory block pointed to by the given pointer.
+   *
+   * @param ptr The pointer to the memory block to be resized.
+   * @param oldSize The current size of the memory block, in bytes.
+   * @param newSize The new size of the memory block, in bytes.
+   * @return A pointer to the resized memory block.
+   */
   virtual void *resize(void *ptr, std::size_t oldSize,
                        std::size_t newSize) override;
+
+  /**
+   * @brief Copies data between host and device memory.
+   *
+   * @param dst The destination pointer for the copy operation.
+   * @param src The source pointer for the copy operation.
+   * @param size The size of the data to be copied, in bytes.
+   * @param dstOnHost Indicates whether the destination memory is on the host.
+   * @param srcOnHost Indicates whether the source memory is on the host.
+   */
   virtual void copy(void *dst, const void *src, std::size_t size,
                     bool dstOnHost, bool srcOnHost) override;
 };
--- src/include/rawvecs.hpp
+++ src/include/rawvecs.hpp
@@ -5,27 +5,66 @@
 
 namespace ChiaVec {
 namespace Raw {
+/**
+ * @brief A simple vector-like class that manages raw memory.
+ *
+ * The `RawVec` class is a simple vector-like class that manages raw memory
+ * using a custom memory allocator. It provides basic functionality for
+ * allocating, resizing, and copying memory, as well as some convenience
+ * methods for working with the raw memory.
+ *
+ * @tparam T The type of elements stored in the vector.
+ * @tparam Allocator The memory allocator to use for managing the raw memory.
+ *                   Defaults to `Memory::DefaultAllocator`.
+ */
 template <class T, class Allocator = Memory::DefaultAllocator>
 class RawVec {
 private:
+  /**
+   * @brief Pointer to the raw memory block.
+   */
   T *elements;
+
+  /**
+   * @brief The length of the raw memory block, in number of elements.
+   */
   size_t length;
 
+  /**
+   * @brief Releases the memory occupied by the raw memory block.
+   */
   void releaseMemory() {
     Allocator allocator;
     allocator.release(elements);
   }
 
+  /**
+   * @brief Deletes the elements in the raw memory block and sets the pointer to
+   * `nullptr`.
+   */
   void deleteElements() {
     releaseMemory();
     elements = nullptr;
   }
 
+  /**
+   * @brief Clears all the elements in the raw memory block and sets the length
+   * to 0.
+   */
   void clearAll() {
     deleteElements();
     length = 0;
   }
 
+  /**
+   * @brief Copies a single element to the raw memory block.
+   *
+   * @param element The element to copy.
+   * @param index The index in the raw memory block where the element should be
+   *              copied.
+   * @param elementOnHost Indicates whether the source element is located on the
+   *                      host (CPU) or the device (GPU).
+   */
   virtual void copyElement(const T &element, std::size_t index,
                            bool elementOnHost) {
     Allocator allocator;
@@ -34,46 +73,83 @@ private:
   }
 
 public:
+  /**
+   * @brief Constructs an empty `RawVec` object.
+   */
   RawVec() : elements(nullptr), length(0) {}
 
+  /**
+   * @brief Constructs a `RawVec` object with the specified length.
+   *
+   * @param length The initial length of the raw memory block.
+   */
   RawVec(std::size_t length) : length(length) {
     Allocator allocator;
     elements = static_cast<T *>(allocator(sizeof(T) * length));
   }
 
+  /**
+   * @brief Constructs a `RawVec` object from a raw memory block.
+   *
+   * @param data The raw memory block to copy.
+   * @param length The length of the raw memory block.
+   * @param onHost Indicates whether the raw memory block is located on the host
+   *               (CPU) or the device (GPU).
+   */
   RawVec(const T *data, std::size_t length, bool onHost) : RawVec(length) {
     copyMemory(data, length, onHost);
   }
 
+  /**
+   * @brief Constructs a `RawVec` object by copying another `RawVec` object.
+   *
+   * @param other The `RawVec` object to copy.
+   */
   template <class OtherAllocator>
   RawVec(const RawVec<T, OtherAllocator> &other)
       : RawVec(other.elements, other.length, OtherAllocator::AllocatesOnHost) {}
 
+  /**
+   * @brief Constructs a `RawVec` object by moving another `RawVec` object.
+   *
+   * @param other The `RawVec` object to move.
+   */
   RawVec(RawVec<T, Allocator> &&other)
       : elements(other.elements), length(other.length) {
     other.elements = nullptr;
     other.length = 0;
   }
 
+  /**
+   * @brief Destroys the `RawVec` object and releases the raw memory block.
+   */
   ~RawVec() { releaseMemory(); }
 
   RawVec<T, Allocator> &operator=(const RawVec<T, Allocator> &other) {
-    Allocator allocator;
-    resize(other.length);
-    allocator.copy(elements, other.elements, sizeof(T) * length,
-                   Allocator::AllocatesOnHost, Allocator::AllocatesOnHost);
+    copyFrom(other);
     return *this;
   }
 
+  /**
+   * @brief Assigns the contents of another `RawVec` object to this `RawVec`
+   * object.
+   *
+   * @param other The `RawVec` object to assign.
+   * @return A reference to the modified `RawVec` object.
+   */
   template <class OtherAllocator>
   RawVec<T, Allocator> &operator=(const RawVec<T, OtherAllocator> &other) {
-    Allocator allocator;
-    resize(other.length);
-    allocator.copy(elements, other.elements, sizeof(T) * length,
-                   Allocator::AllocatesOnHost, OtherAllocator::AllocatesOnHost);
+    copyFrom(other);
     return *this;
   }
 
+  /**
+   * @brief Assigns the contents of another `RawVec` object to this `RawVec`
+   * object by moving.
+   *
+   * @param other The `RawVec` object to move.
+   * @return A reference to the modified `RawVec` object.
+   */
   RawVec<T, Allocator> &operator=(RawVec<T, Allocator> &&other) {
     clearAll();
     elements = other.elements;
@@ -83,12 +159,32 @@ public:
     return *this;
   }
 
+  /**
+   * @brief Returns a pointer to the raw memory block.
+   *
+   * @return A pointer to the raw memory block.
+   */
   T *ptr() { return elements; }
 
+  /**
+   * @brief Returns a const pointer to the raw memory block.
+   *
+   * @return A const pointer to the raw memory block.
+   */
   const T *ptr() const { return elements; }
 
+  /**
+   * @brief Returns the length of the raw memory block, in number of elements.
+   *
+   * @return The length of the raw memory block.
+   */
   std::size_t len() const { return length; }
 
+  /**
+   * @brief Resizes the raw memory block to the specified length.
+   *
+   * @param newLength The new length of the raw memory block.
+   */
   void resize(std::size_t newLength) {
     if (0 == newLength) {
       deleteElements();
@@ -100,6 +196,15 @@ public:
     length = newLength;
   }
 
+  /**
+   * @brief Copies the elements from an iterator range to the raw memory block.
+   *
+   * @tparam Itr The iterator type.
+   * @param iter The beginning of the iterator range to copy.
+   * @param length The number of elements to copy.
+   * @param onHost Indicates whether the source elements are located on the host
+   *               (CPU) or the device (GPU).
+   */
   template <class Itr>
   void copy(Itr iter, std::size_t length, bool onHost) {
     resize(length);
@@ -109,6 +214,15 @@ public:
     }
   }
 
+  /**
+   * @brief Copies the elements from a raw memory block to the raw memory block
+   * of this `RawVec` object.
+   *
+   * @param ptr The pointer to the raw memory block to copy.
+   * @param length The length of the raw memory block to copy.
+   * @param onHost Indicates whether the source memory block is located on the
+   *               host (CPU) or the device (GPU).
+   */
   void copyMemory(const T *ptr, std::size_t length, bool onHost) {
     Allocator allocator;
     resize(length);
@@ -116,18 +230,39 @@ public:
                    Allocator::AllocatesOnHost, onHost);
   }
 
+  /**
+   * @brief Copies the contents of this `RawVec` object to another `RawVec`
+   * object.
+   *
+   * @tparam OtherAllocator The allocator type of the other `RawVec` object.
+   * @param other The `RawVec` object to copy the contents to.
+   */
   template <class OtherAllocator>
   void copyTo(RawVec<T, OtherAllocator> &other) const {
     other.resize(length);
     other.copyMemory(elements, length, Allocator::AllocatesOnHost);
   }
 
+  /**
+   * @brief Copies the contents of another `RawVec` object to this `RawVec`
+   * object.
+   *
+   * @tparam OtherAllocator The allocator type of the other `RawVec` object.
+   * @param other The `RawVec` object to copy the contents from.
+   */
   template <class OtherAllocator>
   void copyFrom(const RawVec<T, OtherAllocator> &other) {
     resize(other.len());
     copyMemory(other.elements, other.len(), OtherAllocator::AllocatesOnHost);
   }
 
+  /**
+   * @brief Creates a clone of this `RawVec` object using a different allocator.
+   *
+   * @tparam OtherAllocator The allocator type to use for the cloned `RawVec`
+   *                        object.
+   * @return A new `RawVec` object that is a clone of this `RawVec` object.
+   */
   template <class OtherAllocator = Allocator>
   RawVec<T, OtherAllocator> clone() const {
     RawVec<T, OtherAllocator> v(length);
