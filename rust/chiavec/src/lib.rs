72ab1d341681d2ca4642e94c02306dd0953c0a92
Chia-Hsiang Cheng <gary.chiahsiang.cheng@gmail.com>
2024-06-02 21:24:14 +0800

    Implement helper functions

--- rust/chiavec/src/lib.rs
+++ rust/chiavec/src/lib.rs
@@ -1,4 +1,5 @@
 use chiavec_sys::*;
+use std::ops::{Index, IndexMut};
 use std::ptr::{addr_of, addr_of_mut, null_mut};
 
 macro_rules! declare_vec {
@@ -8,10 +9,13 @@ macro_rules! declare_vec {
             Vecu8,
             Vec_uint8_t,
             Vec_uint8_t_init,
+            Vec_uint8_t_init_with_values,
             Vec_uint8_t_destroy,
             Vec_uint8_t_len,
             Vec_uint8_t_push,
-            Vec_uint8_t_pop
+            Vec_uint8_t_pop,
+            Vec_uint8_t_get_const,
+            Vec_uint8_t_get
         );
     };
     (u16) => {
@@ -20,10 +24,13 @@ macro_rules! declare_vec {
             Vecu16,
             Vec_uint16_t,
             Vec_uint16_t_init,
+            Vec_uint16_t_init_with_values,
             Vec_uint16_t_destroy,
             Vec_uint16_t_len,
             Vec_uint16_t_push,
-            Vec_uint16_t_pop
+            Vec_uint16_t_pop,
+            Vec_uint16_t_get_const,
+            Vec_uint16_t_get
         );
     };
     (u32) => {
@@ -32,10 +39,13 @@ macro_rules! declare_vec {
             Vecu32,
             Vec_uint32_t,
             Vec_uint32_t_init,
+            Vec_uint32_t_init_with_values,
             Vec_uint32_t_destroy,
             Vec_uint32_t_len,
             Vec_uint32_t_push,
-            Vec_uint32_t_pop
+            Vec_uint32_t_pop,
+            Vec_uint32_t_get_const,
+            Vec_uint32_t_get
         );
     };
     (u64) => {
@@ -44,10 +54,13 @@ macro_rules! declare_vec {
             Vecu64,
             Vec_uint64_t,
             Vec_uint64_t_init,
+            Vec_uint64_t_init_with_values,
             Vec_uint64_t_destroy,
             Vec_uint64_t_len,
             Vec_uint64_t_push,
-            Vec_uint64_t_pop
+            Vec_uint64_t_pop,
+            Vec_uint64_t_get_const,
+            Vec_uint64_t_get
         );
     };
     (i8) => {
@@ -56,10 +69,13 @@ macro_rules! declare_vec {
             Veci8,
             Vec_int8_t,
             Vec_int8_t_init,
+            Vec_int8_t_init_with_values,
             Vec_int8_t_destroy,
             Vec_int8_t_len,
             Vec_int8_t_push,
-            Vec_int8_t_pop
+            Vec_int8_t_pop,
+            Vec_int8_t_get_const,
+            Vec_int8_t_get
         );
     };
     (i16) => {
@@ -68,10 +84,13 @@ macro_rules! declare_vec {
             Veci16,
             Vec_int16_t,
             Vec_int16_t_init,
+            Vec_int16_t_init_with_values,
             Vec_int16_t_destroy,
             Vec_int16_t_len,
             Vec_int16_t_push,
-            Vec_int16_t_pop
+            Vec_int16_t_pop,
+            Vec_int16_t_get_const,
+            Vec_int16_t_get
         );
     };
     (i32) => {
@@ -80,10 +99,13 @@ macro_rules! declare_vec {
             Veci32,
             Vec_int32_t,
             Vec_int32_t_init,
+            Vec_int32_t_init_with_values,
             Vec_int32_t_destroy,
             Vec_int32_t_len,
             Vec_int32_t_push,
-            Vec_int32_t_pop
+            Vec_int32_t_pop,
+            Vec_int32_t_get_const,
+            Vec_int32_t_get
         );
     };
     (i64) => {
@@ -92,10 +114,13 @@ macro_rules! declare_vec {
             Veci64,
             Vec_int64_t,
             Vec_int64_t_init,
+            Vec_int64_t_init_with_values,
             Vec_int64_t_destroy,
             Vec_int64_t_len,
             Vec_int64_t_push,
-            Vec_int64_t_pop
+            Vec_int64_t_pop,
+            Vec_int64_t_get_const,
+            Vec_int64_t_get
         );
     };
     (f32) => {
@@ -104,10 +129,13 @@ macro_rules! declare_vec {
             Vecf32,
             Vec_float,
             Vec_float_init,
+            Vec_float_init_with_values,
             Vec_float_destroy,
             Vec_float_len,
             Vec_float_push,
-            Vec_float_pop
+            Vec_float_pop,
+            Vec_float_get_const,
+            Vec_float_get
         );
     };
     (f64) => {
@@ -116,16 +144,19 @@ macro_rules! declare_vec {
             Vecf64,
             Vec_double,
             Vec_double_init,
+            Vec_double_init_with_values,
             Vec_double_destroy,
             Vec_double_len,
             Vec_double_push,
-            Vec_double_pop
+            Vec_double_pop,
+            Vec_double_get_const,
+            Vec_double_get
         );
     };
 }
 
 macro_rules! implement_vec {
-    ($element_type: ident, $name: ident, $internal: ident, $init: ident, $destroy: ident, $get_len: ident, $push: ident, $pop: ident) => {
+    ($element_type: ident, $name: ident, $internal: ident, $init: ident, $init_with_values: ident, $destroy: ident, $get_len: ident, $push: ident, $pop: ident, $get_const: ident, $get_mut: ident) => {
         pub struct $name {
             data: $internal,
         }
@@ -137,12 +168,22 @@ macro_rules! implement_vec {
                 Self { data }
             }
 
+            fn from_array(values: &[$element_type]) -> Self {
+                let mut data = $internal { _ptr: null_mut() };
+                unsafe { $init_with_values(addr_of_mut!(data), values.as_ptr(), values.len(), 1) };
+                Self { data }
+            }
+
             fn len(&self) -> usize {
                 unsafe { $get_len(addr_of!(self.data)) as usize }
             }
 
-            fn push(&mut self, element: $element_type, on_host: bool) {
-                unsafe { $push(addr_of_mut!(self.data), addr_of!(element), on_host as u8) };
+            fn is_empty(&self) -> bool {
+                self.len() == 0
+            }
+
+            fn push(&mut self, element: $element_type) {
+                unsafe { $push(addr_of_mut!(self.data), addr_of!(element), 1) };
             }
 
             fn pop(&mut self) -> Option<$element_type> {
@@ -153,6 +194,22 @@ macro_rules! implement_vec {
                 let mut element = $element_type::default();
                 unsafe { $pop(addr_of_mut!(self.data), addr_of_mut!(element)) };
                 Some(element)
+            }
+        }
+
+        impl Index<usize> for $name {
+            type Output = $element_type;
+
+            fn index(&self, index: usize) -> &Self::Output {
+                assert!(index < self.len());
+                unsafe { &*$get_const(addr_of!(self.data), index) }
+            }
+        }
+
+        impl IndexMut<usize> for $name {
+            fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+                assert!(index < self.len());
+                unsafe { &mut *$get_mut(addr_of_mut!(self.data), index) }
             }
         }
 
@@ -180,28 +237,28 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_vec_new() {
-        let _vec = Vecu8::new();
-        let _vec = Vecu16::new();
-        let _vec = Vecu32::new();
-        let _vec = Vecu64::new();
-        let _vec = Veci8::new();
-        let _vec = Veci16::new();
-        let _vec = Veci32::new();
-        let _vec = Veci64::new();
-        let _vec = Vecf32::new();
-        let _vec = Vecf64::new();
-    }
+    fn test_vecs() {
+        let mut vec = Veci32::new();
 
-    #[test]
-    fn test_vec_push() {
-        let mut vec = Vecu8::new();
+        assert!(vec.is_empty());
+
         for i in 1..=10 {
-            vec.push(i, true);
+            vec.push(i);
             assert!(vec.len() == i as usize);
+            assert!(vec[i as usize - 1] == i);
         }
         for i in (1..=10).rev() {
             assert!(vec.pop().map_or(false, |e| e == i));
+        }
+
+        assert!(vec.is_empty());
+
+        let vec = Veci32::from_array(&[1, 2, 3, 4, 5]);
+        assert!(!vec.is_empty());
+        assert!(vec.len() == 5);
+
+        for i in 1..=5 {
+            assert!(vec[i - 1] == i as i32);
         }
     }
 }
